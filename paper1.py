77c17e527eca994166e4993e0d606e060bf7941e
Patrick Diehl <me@diehlpk.de>
2019-02-09 20:33:46 -0600

    Add documentation

--- paper1.py
+++ paper1.py
@@ -1,3 +1,8 @@
+# Implementation of the examle problem for 
+# M. Zaccariotto, T. Mudric, D. Tomasi, A. Shojaei, U. Galvanetto, Coupling of fem meshes with peri-
+# dynamic grids, Computer Methods in Applied Mechanics and Engineering 330 (2018) 471–497.
+#@author patrickdiehl@lsu.edu
+#@date 02/09/2019
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -16,7 +21,7 @@ V=h*Area
 c=(2*E)/(Area*delta*delta)
 b=c/h*V*V
 
-# coupled approach
+# Coupling approach presented in the paper
 
 MCoupled = np.array(
  [ [1 , 0 , 0 , 0 , 0, 0 , 0 , 0 , 0 , 0 , 0  ],
@@ -32,7 +37,7 @@ MCoupled = np.array(
   [   0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , -a , a ]])
 
 
-# finite element
+# Pure finite element approach
 
 MFem = np.array(
  [ [1 , 0 , 0 , 0 , 0, 0 , 0 , 0 , 0 , 0 , 0  ],
@@ -47,7 +52,7 @@ MFem = np.array(
   [   0 , 0 , 0 , 0 , 0 , 0 , 0 , 0  , -a , 2*a , -a ],
   [   0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , -a , a ]])
 
-# peridyanmic
+# Pure peridyanmic approach
 
 MPeridynamics = np.array(
  [ [1, 0 , 0 , 0 , 0, 0 , 0 , 0 , 0 , 0 , 0  ],
@@ -70,19 +75,19 @@ f = np.zeros(lenF)
 f[0]=0
 f[len(f)-1]=F
 
-#generate the position in the bar
+# generate the position in the bar
 x= np.arange(0,h*11,0.1)
 
-#solve coupled approach
+# solve coupled approach
 ucoupled = np.linalg.solve(MCoupled,f)
 
-#solve fem approach
+# solve fem approach
 ufem  = np.linalg.solve(MFem,f)
 
-#solve pd approach
+# solve pd approach
 upd  = np.linalg.solve(MPeridynamics,f)
 
-
+# plot the results
 plt.plot(x,ucoupled,label="Coupled")
 plt.plot(x,ufem,label="FEM")
 plt.legend()
