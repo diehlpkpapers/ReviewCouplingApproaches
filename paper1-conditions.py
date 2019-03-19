0f74e070a6f89c8c1f11d69d49a364041b5398e3
Patrick Diehl <me@diehlpk.de>
2019-03-18 19:53:01 -0500

    Add derivatives to paper 1

--- paper1-conditions.py
+++ paper1-conditions.py
@@ -10,8 +10,9 @@ rc('font',**{'family':'sans-serif','sans-serif':['Helvetica']})
 rc('text', usetex=True)
 plt.rcParams.update({'font.size': 15})
 
-
+##############################################################################
 #define non-linear loading functions
+##############################################################################
 
 def f1(x):
     return x;
@@ -25,24 +26,65 @@ def f3(x):
 def f4(x):
     return np.sqrt(x)
 
+def diff(u,x):
+    return np.diff(u) / np.diff(x)
 
+##############################################################################
+#define functions
+##############################################################################
 
+def solve(M,f):
+    return np.linalg.solve(M,f)
+
+def plotu(x,ucoupled,ufem,upd,name,title):
+    plt.plot(x,ucoupled,label="Coupled",lw=2)
+    plt.plot(x,ufem,label="FEM",lw=2)
+    plt.plot(x,upd,label="PD",lw=2)
+    plt.legend()
+    plt.grid()
+    plt.title(title)
+    plt.xlabel("Node position")
+    plt.ylabel("Displacement")
+    plt.savefig(name)
+    plt.cla()
+    
+    
+def plotd(x,diffcoupled,difffem,diffpd,name):
+    plt.plot(x[:-1],diffcoupled,label="Coupled",lw=2)
+    plt.plot(x[:-1],difffem,label="FEM",lw=2)
+    plt.plot(x[:-1],diffpd,label="PD",lw=2)
+    plt.legend()
+    plt.grid()
+    plt.title("$u'(x)$")
+    plt.xlabel("Node position")
+    plt.ylabel("Displacement")
+    plt.savefig(name)
+    plt.cla()
+    
+##############################################################################
 #define properties
+##############################################################################
 E=1
 Area=1
 F=1.0
 
+##############################################################################
 #define the material constant for the finte element discretization
+##############################################################################
 h=0.2409090909090909
 a=E*Area/h
 
+##############################################################################
 #define the material constant for the peridyanmic discretization 
+##############################################################################
 delta=2*h
 V=h*Area
 c=(2*E)/(Area*delta*delta)
 b=c/h*V*V
 
+##############################################################################
 # Coupling approach presented in the paper
+##############################################################################
 
 MCoupled = np.array(
  [ [1 , 0 , 0 , 0 , 0, 0 , 0 , 0 , 0 , 0 , 0  ],
@@ -57,8 +99,9 @@ MCoupled = np.array(
   [   0 , 0 , 0 , 0 , 0 , 0 , 0 , 0  , -a , 2*a , -a ],
   [   0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , -a , a ]])
 
-
+##############################################################################
 # Pure finite element approach
+#############################################################################
 
 MFem = np.array(
  [ [1 , 0 , 0 , 0 , 0, 0 , 0 , 0 , 0 , 0 , 0  ],
@@ -73,7 +116,9 @@ MFem = np.array(
   [   0 , 0 , 0 , 0 , 0 , 0 , 0 , 0  , -a , 2*a , -a ],
   [   0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , -a , a ]])
 
+##############################################################################
 # Pure peridyanmic approach
+##############################################################################
 
 MPeridynamics = np.array(
  [ [1, 0 , 0 , 0 , 0, 0 , 0 , 0 , 0 , 0 , 0  ],
@@ -88,8 +133,10 @@ MPeridynamics = np.array(
   [   0 , 0 , 0 , 0 , 0 , 0 , 0 , 0  , -b , 2*b , -b ],
   [   0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , -b , b ]])
 
- 
- 
+##############################################################################
+#Loading 1
+##############################################################################
+
 # generate the position in the bar
 x= np.arange(0,h*11,h)
 
@@ -107,27 +154,22 @@ for i in range(5,lenF):
 f[0]=0
 
 # solve coupled approach
-ucoupled = np.linalg.solve(MCoupled,f)
+ucoupled = solve(MCoupled,f)
+ufem  = solve(MFem,f)
+upd  = solve(MPeridynamics,f)
 
-# solve fem approach
-ufem  = np.linalg.solve(MFem,f)
-
-# solve pd approach
-upd  = np.linalg.solve(MPeridynamics,f)
-
+#compute first derivative
+difffem = diff(ufem,x) 
+diffpd = diff(upd,x) 
+diffcoupled = diff(ucoupled,x)
 
 # plot the results
-plt.plot(x,ucoupled,label="Coupled",lw=2)
-plt.plot(x,ufem,label="FEM",lw=2)
-plt.plot(x,upd,label="PD",lw=2)
-plt.legend()
-plt.grid()
-plt.title("$f(x)=x$")
-plt.xlabel("Node position")
-plt.ylabel("Displacement")
-plt.savefig("paper1_f1.pdf")
+plotu(x,ucoupled,ufem,upd,"paper1_f1.pdf","$f(x)=x$")
+plotd(x,diffcoupled,difffem,diffpd,"paper1_f1_diff.pdf")
 
-plt.cla()
+##############################################################################
+#Loading 2
+##############################################################################
 
 # generate the left-hand side
 lenF = np.shape(MFem)[0]
@@ -141,28 +183,24 @@ for i in range(5,lenF):
 
 f[0]=0
 
-# solve coupled approach
-ucoupled = np.linalg.solve(MCoupled,f)
-
-# solve fem approach
+# solve the stiffness matric for all approaches
+ucoupled = solve(MCoupled,f)
 ufem  = np.linalg.solve(MFem,f)
-
-# solve pd approach
 upd  = np.linalg.solve(MPeridynamics,f)
+
+#compute first derivative
+difffem = diff(ufem,x) 
+diffpd = diff(upd,x) 
+diffcoupled = diff(ucoupled,x)
 
 
 # plot the results
-plt.plot(x,ucoupled,label="Coupled",lw=2)
-plt.plot(x,ufem,label="FEM",lw=2)
-plt.plot(x,upd,label="PD",lw=2)
-plt.legend()
-plt.grid()
-plt.title("$f(x)=x^2$")
-plt.xlabel("Node position")
-plt.ylabel("Displacement")
-plt.savefig("paper1_f2.pdf")
+plotu(x,ucoupled,ufem,upd,"paper1_f2.pdf","$f(x)=x^2$")
+plotd(x,diffcoupled,difffem,diffpd,"paper1_f2_diff.pdf")
 
-plt.cla()
+##############################################################################
+#Loading 3
+##############################################################################
 
 # generate the left-hand side
 lenF = np.shape(MFem)[0]
@@ -176,28 +214,23 @@ for i in range(5,lenF):
 
 f[0]=0
 
-# solve coupled approach
-ucoupled = np.linalg.solve(MCoupled,f)
+# solve the stiffness matric for all approaches
+ucoupled = solve(MCoupled,f)
+ufem  = solve(MFem,f)
+upd  = solve(MPeridynamics,f)
 
-# solve fem approach
-ufem  = np.linalg.solve(MFem,f)
-
-# solve pd approach
-upd  = np.linalg.solve(MPeridynamics,f)
-
+#compute first derivative
+difffem = diff(ufem,x) 
+diffpd = diff(upd,x) 
+diffcoupled = diff(ucoupled,x)
 
 # plot the results
-plt.plot(x,ucoupled,label="Coupled",lw=2)
-plt.plot(x,ufem,label="FEM",lw=2)
-plt.plot(x,upd,label="PD",lw=2)
-plt.legend()
-plt.grid()
-plt.title("$f(x)=sin(2\pi x)$")
-plt.xlabel("Node position")
-plt.ylabel("Displacement")
-plt.savefig("paper1_f3.pdf")
+plotu(x,ucoupled,ufem,upd,"paper1_f3.pdf","$f(x)=sin(2\pi x)$")
+plotd(x,diffcoupled,difffem,diffpd,"paper1_f3_diff.pdf")
 
-plt.cla()
+##############################################################################
+#Loading 4
+##############################################################################
 
 # generate the left-hand side
 lenF = np.shape(MFem)[0]
@@ -212,24 +245,15 @@ for i in range(5,lenF):
 f[0]=0
 
 # solve coupled approach
-ucoupled = np.linalg.solve(MCoupled,f)
+ucoupled = solve(MCoupled,f)
+ufem  = solve(MFem,f)
+upd  = solve(MPeridynamics,f)
 
-# solve fem approach
-ufem  = np.linalg.solve(MFem,f)
-
-# solve pd approach
-upd  = np.linalg.solve(MPeridynamics,f)
-
+#compute first derivative
+difffem = diff(ufem,x) 
+diffpd = diff(upd,x) 
+diffcoupled = diff(ucoupled,x)
 
 # plot the results
-plt.plot(x,ucoupled,label="Coupled",lw=2)
-plt.plot(x,ufem,label="FEM",lw=2)
-plt.plot(x,upd,label="PD",lw=2)
-plt.legend()
-plt.grid()
-plt.title("$f(x)=\sqrt(x)$")
-plt.xlabel("Node position")
-plt.ylabel("Displacement")
-plt.savefig("paper1_f4.pdf")
-
-plt.cla()
+plotu(x,ucoupled,ufem,upd,"paper1_f4.pdf","$f(x)=\sqrt(x)$")
+plotd(x,diffcoupled,difffem,diffpd,"paper1_f4_diff.pdf")
